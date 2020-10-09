5f68094fd43d54c113497a91369023b1ee1f0159
yeni <moi.yeeun@gmail.com>
2020-10-09 21:25:30 +0900

    part 3

--- djangodocs/mysite/polls/urls.py
+++ djangodocs/mysite/polls/urls.py
@@ -2,13 +2,15 @@ from django.urls import path
 
 from . import views
 
+# 이름공간
+app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name = 'index'),
     # ex: /polls/5
     path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/result
-    path('<intLquestion_id/results', views.results, name='results'),
+    path('<int:question_id>/results', views.results, name='results'),
     # ex: /polls/5/vote
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- djangodocs/mysite/polls/views.py
+++ djangodocs/mysite/polls/views.py
@@ -1,12 +1,31 @@
-from django.http import HttpResponse
+#from django.http import HttpResponse
+#from django.template import loader
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
 
 # Create your views here.
+"""
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+"""
 
+# render()를 이용한 방법. 
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list' : latest_question_list }
+    return render(request, 'polls/index.html', context) 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(reqest, question_id):
     reponse = "Yor're looking at the result of question %s."
